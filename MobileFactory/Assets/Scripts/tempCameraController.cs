5ef2f4f54e75c42c77d4cf02dfd8747fce8dd212
migmig765 <765migmig765@gmail.com>
2018-12-25 10:20:26 +0100

    Project Added
    Added the Project

---
+++ MobileFactory/Assets/Scripts/tempCameraController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tempCameraController : MonoBehaviour {
+
+    public GameObject item;
+    public float cameraSpeed;
+    private bool doOnce = true;
+    private int testvar = 0;
+
+    // Use this for initialization
+    void Start () {
+   
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        TapSelect();
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if(Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                testvar++;
+            }
+            if (Input.GetKeyDown("space") || Input.GetTouch(0).phase == TouchPhase.Ended && testvar < 5)
+            {
+                Instantiate(item, new Vector3(-20.2f, 2.62f, 0.5f), Quaternion.identity);
+            }
+            if (Input.GetTouch(0).phase == TouchPhase.Moved && testvar > 3)
+            {
+                Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+                transform.Translate(-touchDeltaPosition.x * cameraSpeed * Time.deltaTime, 0, 0);
+            }
+            if (Input.GetTouch(0).phase == TouchPhase.Ended)
+            { 
+                testvar = 0;
+            }
+    
+         }
+        
+    }
+
+    void TapSelect()
+    {
+        int layerMask = 1 << 10;
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask, QueryTriggerInteraction.Collide))
+                {
+                    Debug.Log("Did Hit");
+                }
+            }
+        }
+    }
+
+
+}
